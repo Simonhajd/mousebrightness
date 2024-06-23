54737e3cda9cb635121b19cb64d51cf67b6ad7e4
Simon Hajduk <simonhajduk@icloud.com>
2024-06-23 23:56:37 +0200

    added a volume view, watermark, and more compatibility

--- controltest.py
+++ controltest.py
@@ -1,34 +1,57 @@
 import tkinter as tk
 from screeninfo import get_monitors
-
+import pyautogui
+from tkinter import ttk
+import PIL
+from PIL import Image, ImageTk
 def create_highlighted_edge(window, x, y, width, height):
-
     window.canvas.create_rectangle(x, y, x + width, y + height, fill="red", stipple="gray50")
+
+def update_progress_bar(progressbar, screen_width, screen_height):
+    x, y = pyautogui.position()
+    progress = 100 - (y / (screen_height / 100))
+    progressbar['value'] = progress
+    progressbar.after(1, update_progress_bar, progressbar, screen_width, screen_height)
+
 
 def main():
     root = tk.Tk()
-    root.attributes('-alpha', 0.5) 
-    root.attributes('-topmost', True) 
-    root.overrideredirect(True) 
+    root.attributes('-alpha', 0.5)
+    root.attributes('-topmost', True)
+    root.overrideredirect(True)
 
     monitor = get_monitors()[0]
     screen_width = monitor.width
     screen_height = monitor.height
 
-    
     root.geometry(f"{screen_width}x{screen_height}+0+0")
 
-   
     canvas = tk.Canvas(root, bg='blue', highlightthickness=0)
     canvas.pack(fill=tk.BOTH, expand=True)
     root.canvas = canvas
 
-   
-    edge_width = 10  
-    create_highlighted_edge(root, 0, 0, screen_width, edge_width) 
-    create_highlighted_edge(root, 0, screen_height - edge_width, screen_width, edge_width)  
-    create_highlighted_edge(root, 0, 0, edge_width, screen_height) 
-    create_highlighted_edge(root, screen_width - edge_width, 0, edge_width, screen_height) 
+
+    PIL.Image.open("mono.png")
+    image = PIL.Image.open("mono.png")
+    image = image.resize((100, 100))  # adjust the size as needed
+    photo = PIL.ImageTk.PhotoImage(image)
+    canvas.create_image(50, 50, anchor=tk.NW, image=photo)
+
+    progressbar = ttk.Progressbar(root, orient=tk.VERTICAL, length=160, style="green.Horizontal.TProgressbar") 
+    progressbar.place(x=(screen_width / 2), y=(screen_height / 6))
+
+
+    style = ttk.Style()
+    style.theme_use('default')
+    style.configure("green.Horizontal.TProgressbar", background='#FFFF00')
+
+    update_progress_bar(progressbar, screen_width, screen_height) 
+
+    edge_width = 20
+    create_highlighted_edge(root, 0, 0, screen_width, edge_width)
+    create_highlighted_edge(root, 0, screen_height - edge_width, screen_width, edge_width)
+    create_highlighted_edge(root, 0, 0, edge_width, screen_height)
+    create_highlighted_edge(root, screen_width - edge_width, 0, edge_width, screen_height)
 
     root.mainloop()
 
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pynput import keyboard
 import subprocess
 import os
 from playsound import playsound
+from screeninfo import get_monitors
 
 user_selection = None
 service = input("\nEnter S for Spotify | Enter A for Apple Music\n")
@@ -14,7 +15,7 @@ def on_press(key):
     global x
     try:
         if key == keyboard.Key.space:
-            print(service)
+            
             subprocess.run(("open -a "+service), shell=True)
     except Exception as e:
         print(f"Error: {e}")
@@ -27,42 +28,50 @@ if service == "A" or service == "a":
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
 
+monitor = get_monitors()[0]
+screen_width = monitor.width
+screen_height = monitor.height
 while True: 
+
     x, y = pyautogui.position()
     positionStr = ('X' + str(x)).rjust(4)
-    y = 100 - (y/9.55)
-    x = 100 - (x/14.69)
-    print(positionStr, end='')
-    osascript.osascript("set volume output volume "+str(y))
-    print('\b' * len(positionStr), end='', flush=True) 
-    print("| x: ", x, end='\r')
-    if x <= 2:
-        print("Skip detected")
+    y = 100 - (y/(screen_height/100))
+
+    print(x, end='')
+
+    if x <= 20:
+      
         playsound('funk.aiff', block=False)
-        print("Sound played")
+        pyautogui.moveTo((screen_width/2), None)
+
         # Use pynput for next track
         controller.press(keyboard.Key.media_next)
-        print("key pressed")
+
         controller.release(keyboard.Key.media_next)
-        print("key released")
-        pyautogui.moveTo(734, None)
-        print("Moved mouse")
+   
         print("Next")
         
-    if x >= 98:
+    if x >= screen_width-20:
         os.system(f'afplay funk.aiff')
+        pyautogui.moveTo((screen_width/2), None)
         # Use pynput for previous track
         controller.press(keyboard.Key.media_previous)
         controller.release(keyboard.Key.media_previous)
-        pyautogui.moveTo(734, None)
+        
         print("Prev")
         
     if y <= 2:
         os.system(f'afplay funk.aiff')
+        pyautogui.moveTo(None, (screen_height/2))
         # Use pynput for play/pause
         controller.press(keyboard.Key.media_play_pause)
         controller.release(keyboard.Key.media_play_pause)
+        
         print("Paused/Played")
+    osascript.osascript("set volume output volume "+str(y))
+    print('\b' * len(positionStr), end='', flush=True) 
+    print("| x: ", x, end='\r')
+    
         
 
 
